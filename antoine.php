3cd2217c68cb4be52786eeab58e1c3a820501cd0
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2022-09-15 12:18:13 +0200

    🔧 switch html to PHP

---
+++ antoine.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antoine</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>
+        antoine
+    </h1>
+
+    <div class="container" id="blur">
+        <div class="card">
+           <div class="containt">
+              <h2>Antoine</h2>
+              <p> </p>
+              <a href="#" onclick="toggle()">info</a>
+     
+           </div>
+           <img src="image/baam.png" alt="">
+        </div>
+     </div>
+     
+        <div id="popup">
+           <h2>baam and rachel story</h2>
+           <p>wenty-Fifth Baam or simply Baam for short, is an Irregular and the main protagonist of Tower of God. <br> Baam entered the Tower in search of his best friend Rachel, another Irregular, who wanted to climb to the top of the Tower and see the stars. <br> Prior to passing Headon's test, he was deemed worthy of climbing the Tower. <br> Before ascending, however, Baam is required to pass the Testing Floors which are there to weed out anyone that could be harmful to the Tower, <br> and he puts together a strong team in the process. After later being forcibly recruited into FUG, Baam joined Team Tangsooyook and managed to escape from FUG's clutches, <br> eventually reuniting with his old friends and teammates. <br> Due to his affiliations with FUG, he also goes by the name Jue Viole Grace. <br> After the battle of the last station, he decided to return to FUG in order to become more powerful to track down Kallavan and prepare for the war against his army.</p>
+           <p>Rachel , also known as Michelle Light  is an Irregular and was Baam's best and only friend <br> as she was the only one to visit him in his "cave". <br> She longs to see the stars which is one of the fabled rewards waiting at the top of the Tower, because she "fears the Night". <br> Having once been a member of Khun's team, she now works alongside FUG. <br> After the events that took place on the Last Station, she was last seen being taken to the floating castle of Po Bidau Family, along with Ha Yura.</p>
+           <a href="#" onclick="toggle()">Close</a>
+        </div>
+        
+     
+     
+     
+     
+     
+     
+      
+    
+</body>
+</html>
