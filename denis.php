992c12d44f96927f588e68b1dd4edb11d5969a77
tinkode92 <tinkod@MacBook-Pro-de-Yanis-2.local>
2022-09-16 11:25:35 +0200

    :wrench:add php

---
+++ denis.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Denis</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>
+        Denis
+    </h1>
+    <div class="container" id="blur">
+        <div class="card">
+           <div class="containt">
+              <h2>Denis</h2>
+              <p> </p>
+              <a href="#" onclick="toggle()">info</a>
+     
+           </div>
+           <img src="image/baam.png" alt="">
+        </div>
+     </div>
+     
+        <div id="popup">
+           <h2> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, commodi! Dolor praesentium corporis sequi nemo deleniti, alias consequatur! Nisi, iusto sequi eius possimus perspiciatis doloremque odio fugiat odit enim adipisci!</h2>
+           <p>lor minima excepturisapiente culpa placeat similique! <br>  FUG's clutches, <br> eventually reuniting with his old friends and teammates. <br> Due to his affiliations with FUG, he also goes by the name Jue Viole Grace. <br> After t become more powerful to against his army.</p>
+           <p>lala , a the Tower, because she "fears the Night". <br> Having once been a member of Khun's team, she now works alongside FUG. <br> After the with Ha Yura.</p>
+           <a href="#" onclick="toggle()">Close</a>
+        </div>
+        
+     
+     
+     
+     
+     
+     
+      
+    
+</body>
+</html>
