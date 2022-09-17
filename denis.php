c51bd9b3772817e59ec9b32217c04e76c6368246
tinkode92 <tinkod@MBP-de-Yanis.lan>
2022-09-17 21:44:14 +0200

    :wrench: html to php

--- denis.php
+++ denis.php
@@ -19,25 +19,13 @@
               <a href="#" onclick="toggle()">info</a>
      
            </div>
-           <img src="image/baam.png" alt="">
+           <img src="image/denistete.jpg" alt="">
         </div>
      </div>
      
         <div id="popup">
-           <h2> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, commodi! Dolor praesentium corporis sequi nemo deleniti, alias consequatur! Nisi, iusto sequi eius possimus perspiciatis doloremque odio fugiat odit enim adipisci!</h2>
-           <p>lor minima excepturisapiente culpa placeat similique! <br>  FUG's clutches, <br> eventually reuniting with his old friends and teammates. <br> Due to his affiliations with FUG, he also goes by the name Jue Viole Grace. <br> After t become more powerful to against his army.</p>
-           <p>lala , a the Tower, because she "fears the Night". <br> Having once been a member of Khun's team, she now works alongside FUG. <br> After the with Ha Yura.</p>
+           <h2>Pres</h2>
+           <p>Pres<br> Pres <br> Pres <br> Pres, <br> Pres</p>
+           <p> Pres <br> lore. <br>  pres. <br> Hpres. <br> pres</p>
            <a href="#" onclick="toggle()">Close</a>
         </div>
-        
-     
-     
-     
-     
-     
-     
-      
-        <script type="text/javascript" src="script.js"></script>
-
-</body>
-</html>
