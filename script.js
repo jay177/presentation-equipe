dffeab23475707b79561026109126fa4512e0f27
tinkode92 <tinkod@MBP-de-Yanis.lan>
2022-09-17 21:48:51 +0200

    :dizzy: add js

---
+++ script.js
@@ -0,0 +1,17 @@
+let imgbx = document.querySelectorAll('.imgbx')
+imgbx.forEach(popup => popup.addEventListener('click', () => 
+{
+    popup.classList.toggle('active')
+}))
+
+
+
+
+
+function toggle(){
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup')
+    popup.classList.toggle('active');
+
+}
