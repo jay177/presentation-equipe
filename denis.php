a12d255d2abcd4dcbcc4086edfe422c47acbc772
tinkode92 <tinkod@MBP-de-Yanis.lan>
2022-09-17 22:08:26 +0200

    :sparkles: add a footer

--- denis.php
+++ denis.php
@@ -29,7 +29,17 @@
            <p> Pres <br> lore. <br>  pres. <br> Hpres. <br> pres</p>
            <a href="#" onclick="toggle()">Close</a>
         </div>
-
+        <footer>
+         <div class="footer">
+            <h2>Contact</h2>
+            <p>N'ayez pas peur de le contacter, il ne vous mordra pas !</p>
+            <ul class="social_media">
+               <li><a href="#"><i class="fa fa-linkedin""></i></a></li>
+               <li><a href="#"><i class="fa fa-instagram""></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter""></i></a></li>
+            </ul>
+         </div>
+      </footer>
         <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
