caaaa4d77d3b3e7ea0e96d95354c3d0a18498c43
antoinebayssac <antoine.bayssac@edu.devinci.fr>
2022-09-17 20:57:15 +0200

    :speech_balloon: added information from Jarrel

--- jarrel2.php
+++ jarrel2.php
@@ -3,11 +3,16 @@
 <head>	
     <meta charset="UTF-8">	
     <meta http-equiv="X-UA-Compatible" content="IE=edge">	
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">	
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     	
 </head>	
 <body>
-    <?php include_once 'PHP/menu.php'; ?>	
+    <?php include_once 'PHP/menu.php'; ?>
+    
+    <h1>Présentation de Jarrel</h1>
+    <h2>L'histoire de Yanis </h2>
+         <p>Jarrel à 21 ans et étudie à l'IIM Digital School en Coding & Digital Innovation afin de pouvoir devenir Chef De Projet</p>	   
 
 </body>	
     	
