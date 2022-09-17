20896b5ec08daa42bdb9c3d07cbf51119f2add57
antoinebayssac <antoine.bayssac@edu.devinci.fr>
2022-09-17 20:43:44 +0200

    :wrench: add navbar in jarrel2

--- jarrel2.php
+++ jarrel2.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">	
     	
 </head>	
-<body>	
+<body>
+    <?php include_once 'PHP/menu.php'; ?>	
+
 </body>	
     	
 </body>	
