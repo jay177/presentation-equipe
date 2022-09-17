7e567a2d8dd31b815876eea1c0a89d7a536af034
tinkode92 <tinkod@MBP-de-Yanis.lan>
2022-09-17 21:51:57 +0200

    :fire: removejs and add another

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-let imgbx = document.querySelectorAll('.imgbx')
-imgbx.forEach(popup => popup.addEventListener('click', () => 
-{
-    popup.classList.toggle('active')
-}))
-
-
-
-
-
 function toggle(){
     var blur = document.getElementById('blur')
     blur.classList.toggle('active');
     var popup = document.getElementById('popup')
     popup.classList.toggle('active');
-
-}
