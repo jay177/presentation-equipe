37cca3befb253de01c12a647d52ef5dd26f37919
antoinebayssac <antoine.bayssac@edu.devinci.fr>
2022-09-18 16:51:30 +0200

    :dizzy:add more js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ imgbx.forEach(popup => popup.addEventListener('click', () =>
     popup.classList.toggle('active')
 }))
 
+function toggle(){
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup')
+    popup.classList.toggle('active');
+
+}
