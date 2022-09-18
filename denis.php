49e2a3a50b3859b507c4113a593ce7dfcec3ad9d
tinkode92 <tinkod@MBP-de-Yanis.lan>
2022-09-18 22:10:26 +0200

    :memo: add content presentation denis

--- denis.php
+++ denis.php
@@ -1,22 +1,36 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Denis</title>
-    <link rel="stylesheet" href="style.css">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Denis Bucspun</title>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+   <link rel="stylesheet" href="style.css">
+   
 </head>
+<header class="header">
+            
+            <ul class="menu">
+                <li>
+                    <a href="antoine.php" data-text="Antoine">Antoine</a>
+                </li>
+                <li>
+                    <a href="jarrel2.php" data-text="Jarrel">Jarrel</a>
+                </li>
+                <li>
+                    <a href="denis.php" data-text="Denis">Denis</a>
+                </li>
+                <li>
+                    <a href="yanis.php" data-text="Yanis">Yanis</a>
+                </li>
+            </ul>
+</header>
 <body>
-    <h1>
-        Denis
-    </h1>
-    <div class="container" id="blur">
+   <h1>
+   </h1>
+   <div class="container" id="blur">
         <div class="card">
            <div class="containt">
               <h2>Denis</h2>
               <p> </p>
-              <a href="#" onclick="toggle()">info</a>
+              <a href="#" onclick="toggle()">Information</a>
      
            </div>
            <img src="image/denistete.jpg" alt="">
@@ -24,12 +38,15 @@
      </div>
      
         <div id="popup">
-           <h2>Pres</h2>
-           <p>Pres<br> Pres <br> Pres <br> Pres, <br> Pres</p>
-           <p> Pres <br> lore. <br>  pres. <br> Hpres. <br> pres</p>
-           <a href="#" onclick="toggle()">Close</a>
+           <h2>Presentation Denis Bucspun</h2>
+           <br></br>
+           <p>Denis à 20 ans et étudie à l'IIM Digital School en Coding & Digital Innovation afin de pouvoir devenir Developpeur Full Stack.</p>
+           <p>Passionné de sport mais plus particulièrement le volley c'est un adepte du smatch ! Denis est une personne à l'écoute et sympathique.</p>
+           <p>Denis souhaite créer son propre site internet pour pouvoir promouvoir Osu est un jeu de rythme gratuit et open source pour Windows, Mac OS et Linux, créé par Dean.</p>
+           <br>
+           <a href="#" onclick="toggle()">Fermer</a>
         </div>
-        <footer>
+      <footer>
          <div class="footer">
             <h2>Contact</h2>
             <p>N'ayez pas peur de le contacter, il ne vous mordra pas !</p>
@@ -40,6 +57,6 @@
             </ul>
          </div>
       </footer>
-        <script type="text/javascript" src="script.js"></script>
-</body>
+      <script type="text/javascript" src="script.js"></script>
+</body>	
 </html>
