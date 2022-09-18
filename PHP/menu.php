523c733fb65d6664d9eb6fb8350e2bdb74c12941
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2022-09-18 23:37:40 +0200

    🔧 menu fix

--- PHP/menu.php
+++ PHP/menu.php
@@ -15,13 +15,13 @@
                     <a href="./antoine.php" data-text="Antoine">Antoine</a>
                 </li>
                 <li>
-                    <a href="jarrel2.php" data-text="Jarrel">Jarrel</a>
+                    <a href="./jarrel2.php" data-text="Jarrel">Jarrel</a>
                 </li>
                 <li>
-                    <a href="denis.php" data-text="Denis">Denis</a>
+                    <a href="./denis.php" data-text="Denis">Denis</a>
                 </li>
                 <li>
-                    <a href="yanis.php" data-text="Yanis">Yanis</a>
+                    <a href="./yanis.php" data-text="Yanis">Yanis</a>
                 </li>
             </ul>
 </header>
