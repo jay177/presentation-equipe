e1e4d8a837a38f807e575a805ff6a51dc8523fad
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2022-09-18 21:08:54 +0200

    💬update and add content on antoine.php

--- antoine.php
+++ antoine.php
@@ -28,8 +28,9 @@
         <div id="popup">
            <h2>L'histoire de Antoine </h2>
            <p>j'ai connue antoine il n'ya que quelque jour a la rentrée du coup nous avons fait des traveaux ensemble</p>
-           <p>donc j'apprend tourjour a le connaitre je ne sait pour l'instant pas grand chose mais je sais qu'il vien du même lycée </p>
-           <p>que yanis eux ce connaise </p>
+           <p>donc j'apprend tourjour a le connaitre je ne sait pour l'instant pas grand chose mais je sais que lui et yanis vienne du même lycée </p>
+           <p>j'ai récament qu'il aimait beaucoup la musique </p>
+           
            <a href="#" onclick="toggle()">Close</a>
         </div>
         
