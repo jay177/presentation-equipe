fb8c4d792c01078113066f588b64c7b2a03dfa76
tinkode92 <tinkod@MBP-de-Yanis.lan>
2022-09-18 21:12:57 +0200

    :hammer: add page navbar

---
+++ PHP/menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Document</title>
+</head>
+<body>
+<header class="header">
+            
+            <ul class="menu">
+                <li>
+                    <a href="antoine.php" data-text="Antoine">Antoine</a>
+                </li>
+                <li>
+                    <a href="jarrel2.php" data-text="Jarrel">Jarrel</a>
+                </li>
+                <li>
+                    <a href="denis.php" data-text="Denis">Denis</a>
+                </li>
+                <li>
+                    <a href="yanis.php" data-text="Yanis">Yanis</a>
+                </li>
+            </ul>
+</header>
+    
+</body>
